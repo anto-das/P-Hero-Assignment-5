2f56f330f2de95a87426e288ac3f4350d6cb79b0
anto_das <antuvhai@gmail.com>
2024-09-23 01:21:10 +0600

    nav-part

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('blog-btn').addEventListener('click',function(){
+     document.getElementById('blog-btn').classList.add('hidden');
+     window.location.href='navbar.html';
+})
---
+++ js/nav.js
@@ -0,0 +1,4 @@
+document.getElementById('home-btn').addEventListener('click',function(){
+    document.getElementById('home-btn').classList.add('hidden');
+    history.back()
+})
