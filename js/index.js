61920dd2fe82d616120460c63b863cc3ea2ad38f
anto_das <antuvhai@gmail.com>
2024-09-23 19:10:42 +0600

    fourth-commit

--- js/index.js
+++ js/index.js
@@ -3,9 +3,7 @@ document.getElementById('blog-btn').addEventListener('click',function(){
      document.getElementById('blog-btn').classList.add('hidden');
      window.location.href='blog.html';
 })
- 
-// input-value string to number convert
-
+// card-1 function start
 document.getElementById('donate-one')
 .addEventListener('click', function(event){
      event.preventDefault();
@@ -13,36 +11,64 @@ document.getElementById('donate-one')
      // balance amount count start
  let campOneBalance = getInnerTextById('camp-one-balance');
  let mainBalance =getInnerTextById('main-balance');
- if(inputValue > 0){
+if(inputValue > 0){
       let newMainBalance = mainBalance - inputValue;
  document.getElementById('main-balance').innerText =newMainBalance;
  let newCampBalance = campOneBalance + inputValue;
  document.getElementById('camp-one-balance').innerText =newCampBalance;
+ let modalContent = document.getElementById('my_modal_3');
+ modalContent.onclick=my_modal_3.showModal();
  }
 
  else{
-       //  create Element start
-let div = document.createElement('div');
-div.innerHTML = `
-<dialog id="my_modal_3" class="modal">
-             <div class="modal-box">
-               <h3 class="text-lg font-bold">Hello!</h3>
-               <p class="py-4">Press ESC key or click on ✕ button to close</p>
-               <form method="dialog">
-                 <button class="btn">Close Confirmation</button>
-               </form>
-             </div>
-           </dialog>
-`;
-document.getElementById('container').appendChild(div);
-// create Element end
-     alert('Please Input a Valided Number');
+     alert('Please Input a Valid Number');
+ }
+})
+// card-function-end
+
+// card-2 function start
+document.getElementById('donate-two')
+.addEventListener('click', function(event){
+     event.preventDefault();
+     let inputValue =  getInputValueById('input-two');
+     // balance amount count start
+ let campOneBalance = getInnerTextById('camp-two-balance');
+ let mainBalance =getInnerTextById('main-balance');
+if(inputValue > 0){
+      let newMainBalance = mainBalance - inputValue;
+ document.getElementById('main-balance').innerText =newMainBalance;
+ let newCampBalance = campOneBalance + inputValue;
+ document.getElementById('camp-two-balance').innerText =newCampBalance;
+ let modalContent = document.getElementById('my_modal_2');
+ modalContent.onclick=my_modal_3.showModal();
  }
 
-
-
-
-
-// balance amount count end
+ else{
+     alert('Please Input a Valid Number');
+ }
 })
+// card-2 function end
 
+
+// card-3 function start
+document.getElementById('donate-three')
+.addEventListener('click', function(event){
+     event.preventDefault();
+     let inputValue =  getInputValueById('input-three');
+     // balance amount count start
+ let campOneBalance = getInnerTextById('camp-three-balance');
+ let mainBalance =getInnerTextById('main-balance');
+if(inputValue > 0){
+      let newMainBalance = mainBalance - inputValue;
+ document.getElementById('main-balance').innerText =newMainBalance;
+ let newCampBalance = campOneBalance + inputValue;
+ document.getElementById('camp-three-balance').innerText =newCampBalance;
+ let modalContent = document.getElementById('my_modal_1');
+ modalContent.onclick=my_modal_3.showModal();
+ }
+
+ else{
+     alert('Please Input a Valid Number');
+ }
+})
+// card-3 function end
