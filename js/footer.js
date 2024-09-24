9292f335acd3d204eee73864455ccb1c803d7c67
anto_das <antuvhai@gmail.com>
2024-09-25 01:37:45 +0600

    footer-check

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+// about-us function start
+document.getElementById('about-us').addEventListener('click',function(){
+    document.getElementById('blog-btn').classList.add('hidden');
+    window.location.href='blog.html';
+})
+// about-us function end
